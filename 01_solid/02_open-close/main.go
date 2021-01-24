bf0653cfbd8c4dfde0765481800c8ced16e4ac1e
Ahmad Berahman <62759025+a-berahman@users.noreply.github.com>
2021-01-24 19:58:23 +0330

    adding open/close

---
+++ 01_solid/02_open-close/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Sword struct {
+	name string // Important tip for RPG players: always name your swords!
+}
+
+// Damage returns the damage dealt by this sword.
+func (Sword) Damage() int {
+	return 2
+}
+
+// String implements fmt.Stringer for the Sword type.
+func (s Sword) String() string {
+	return fmt.Sprintf("%s is a sword that can deal %d points to oppnents", s.name, s.Damage())
+}
+
+type MagicSword struct {
+	// Embed the Sword type
+	Sword
+}
+
+// Damage returns the damage dealt by the enchanted sword.
+func (MagicSword) Damage() int {
+	return 42
+}
