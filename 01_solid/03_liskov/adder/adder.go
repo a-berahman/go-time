69357729930311b98365c1f5dfffa99d34f933ff
Ahmad Berahman <62759025+a-berahman@users.noreply.github.com>
2021-01-28 19:27:07 +0330

    adding liskov

--- 01_solid/03_liskov/adder/adder.go
+++ 01_solid/03_liskov/adder/adder.go
@@ -1,13 +1,13 @@
 package adder
 
+// Int adds two integer values.
 type Int struct{}
 
-func (Int) Add(a, b int) int {
-	return a + b
-}
+// Add returns the sum a+b.
+func (Int) Add(a, b int) int { return a + b }
 
+// Double adds two double values.
 type Double struct{}
 
-func (Double) Add(a, b float64) float64 {
-	return a + b
-}
+// Add returns the sum a+b.
+func (Double) Add(a, b float64) float64 { return a + b }
--- 01_solid/03_liskov/main.go
+++ 01_solid/03_liskov/main.go
@@ -15,6 +15,13 @@ func PrintSum(a, b int, adder Adder) {
 }
 
 func main() {
-	PrintSum(2, 3, adder.Int{})
-	PrintSum(2, 3, adder.Double{})
+	PrintSum(1, 2, adder.Int{}) // prints: "1 + 2 = 3"
+
+	// This line will trigger a compile-time error:
+	//  cannot use adder.Double literal (type adder.Double) as type Adder
+	//  in argument to PrintSum: adder.Double does not implement Adder
+	//  (wrong type for Add method)
+	//      have Add(float64, float64) float64
+	//      want Add(int, int) int
+	//PrintSum(1, 2, adder.Double{})
 }
